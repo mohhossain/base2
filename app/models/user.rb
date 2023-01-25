8daf6570cc854573ff8a2a87bcdbcdc8ad1611ef
Mohammad <moh.hossain98@gmail.com>
2023-01-25 14:19:38 -0500

    friend self join test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_many :questions
     has_many :answers
     has_many :questions, through: :answers
+    has_many :friendships
+    has_many :friends, through: :friendships
     has_secure_password
 end
